87506d8b2794034d6ec7dd9f23dea434d49ac0d5
jasimuddin123 <jasimuddindxn@gmail.com>
2020-06-13 13:25:13 -0700

    26.5 INSTALL FIREBASE, INITIALIZE FIREBASE APP && 26-6 Sign in with google, open google login popup && 26-7 Set logged in user in state, display logged in user info

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+import './App.css';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+firebase.initializeApp(firebaseConfig);
+
+function App() {
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name:'',
+    email:'',
+    photo:''
+
+  })
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleSignIn = ()=>{
+    firebase.auth().signInWithPopup(provider)
+    .then(res =>{
+      const {displayName, photoURL,email} = res.user;
+      const signedIndUser ={
+        isSignedIn:true,
+        name:displayName,
+        email:email,
+        photo:photoURL
+      }
+      setUser(signedIndUser);
+      console.log(displayName, photoURL, email );
+    })
+    .catch(err =>{
+      console.log(err);
+      console.log(err.message)
+    })
+  }
+  return (
+    <div className="App">
+      <button style={{backgroundColor:'green', width:'150px',marginTop:'400px'}} onClick={handleSignIn}>Sign In</button>
+      {
+        user.isSignedIn &&<div>
+         <p> Welcome ,{user.name}</p>
+         <p>Your Email:{user.email}</p>
+         <img src={user.photo} alt=""/>
+         </div>
+      }
+    </div>
+    // This is for upload git 
+    // for Checking git
+  );
+}
+
+export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDC3IPSAJdCRT9Z5jO2FvmlvkWLEOFxcwk",
+    authDomain: "ema-jhon-jasim.firebaseapp.com",
+    databaseURL: "https://ema-jhon-jasim.firebaseio.com",
+    projectId: "ema-jhon-jasim",
+    storageBucket: "ema-jhon-jasim.appspot.com",
+    messagingSenderId: "338121624119",
+    appId: "1:338121624119:web:5ca2f57c1f2930d0c9cdb2"
+  };
+  
+  export default firebaseConfig;
